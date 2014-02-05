d72619b20fc4a9d70b25761c25bb3e3edd1a288b
Ross Riley <riley.ross@gmail.com>
2014-02-05 08:57:57 +0000

    added in phpunit bootstrap and moved autoload to PSR-4

---
+++ lib/Formatter.php
@@ -0,0 +1,112 @@
+<?php
+namespace RomanNumber;
+
+class Formatter {
+
+	protected $symbolMap = array(
+		array( 'I', 'V' ),
+		array( 'X', 'L' ),
+		array( 'C', 'D' ),
+		array( 'M' ),
+	);
+
+	public function __construct( array $symbolMap = array() ) {
+		if ( $symbolMap !== array() ) {
+			$this->symbolMap = $symbolMap;
+		}
+	}
+
+	public function formatNumber( $number ) {
+		$this->ensureNumberIsAnInteger( $number );
+		$this->ensureNumberIsWithinBounds( $number );
+		return $this->constructRomanString( $number );
+	}
+
+	protected function ensureNumberIsAnInteger( $number ) {
+		if ( !is_int( $number ) ) {
+			throw new \InvalidArgumentException( 'Can only translate integers to roman' );
+		}
+	}
+
+	protected function ensureNumberIsWithinBounds( $number ) {
+		if ( $number < 1 ) {
+			throw new \OutOfRangeException( 'Numbers under one cannot be translated to roman' );
+		}
+
+		if ( $number > $this->getUpperBound() ) {
+			throw new \OutOfBoundsException( 'The provided number is to big to be fully translated to roman' );
+		}
+	}
+
+	public function getUpperBound() {
+		$symbolGroupCount = count( $this->symbolMap );
+		$valueOfOne = pow( 10, $symbolGroupCount - 1 );
+
+		$hasFiveSymbol = array_key_exists( 1, $this->symbolMap[$symbolGroupCount - 1] );
+
+		return $valueOfOne * ( $hasFiveSymbol ? 9 : 4 ) - 1;
+	}
+
+	protected function constructRomanString( $number ) {
+		$romanNumber = '';
+
+		for ( $i = 0; $i < count( $this->symbolMap ) ; $i++ ) {
+			$divisor = pow( 10, $i + 1 );
+			$remainder = $number % $divisor;
+			$digit = $remainder / pow( 10, $i );
+
+			$number -= $remainder;
+			$romanNumber = $this->formatDigit( $digit, $i ) . $romanNumber;
+
+			if ( $number === 0 ) {
+				break;
+			}
+		}
+
+		return $romanNumber;
+	}
+
+	protected function formatDigit( $digit, $orderOfMagnitude ) {
+		if ( $digit === 0 ) {
+			return '';
+		}
+
+		if ( $digit === 4 || $digit === 9 ) {
+			return $this->formatFourOrNine( $digit, $orderOfMagnitude );
+		}
+
+		$romanNumber = '';
+
+		if ( $digit >= 5 ) {
+			$digit -= 5;
+			$romanNumber .= $this->getFiveSymbol( $orderOfMagnitude );
+		}
+
+		$romanNumber .= $this->formatOneToThree( $orderOfMagnitude, $digit );
+
+		return $romanNumber;
+	}
+
+	protected function formatFourOrNine( $digit, $orderOfMagnitude ) {
+		$firstSymbol = $this->getOneSymbol( $orderOfMagnitude );
+		$secondSymbol = $digit === 4 ? $this->getFiveSymbol( $orderOfMagnitude ) : $this->getTenSymbol( $orderOfMagnitude );
+		return $firstSymbol . $secondSymbol;
+	}
+
+	protected function formatOneToThree( $orderOfMagnitude, $digit ) {
+		return str_repeat( $this->getOneSymbol( $orderOfMagnitude ), $digit );
+	}
+
+	protected function getOneSymbol( $orderOfMagnitude ) {
+		return $this->symbolMap[$orderOfMagnitude][0];
+	}
+
+	protected function getFiveSymbol( $orderOfMagnitude ) {
+		return $this->symbolMap[$orderOfMagnitude][1];
+	}
+
+	protected function getTenSymbol( $orderOfMagnitude ) {
+		return $this->symbolMap[$orderOfMagnitude + 1][0];
+	}
+
+}
---
+++ tests/FormatterTest.php
@@ -0,0 +1,272 @@
+<?php
+namespace RomanNumber\Tests;
+
+use RomanNumber\Formatter;
+
+class FormatterTest extends \PHPUnit_Framework_TestCase
+{
+
+	public function testCanConstruct() {
+		new Formatter();
+		$this->assertTrue( true );
+	}
+
+	/**
+	 * @dataProvider simpleNumberProvider
+	 */
+	public function testFormatSimpleNumber( $expected, $simpleNumber ) {
+		$this->assertInputResultsInExpected( $expected, $simpleNumber );
+	}
+
+	protected function assertInputResultsInExpected( $expected, $input ) {
+		$formatter = new Formatter();
+		$result = $formatter->formatNumber( $input );
+
+		$this->assertInternalType( 'string', $result );
+		$this->assertEquals( $expected, $result );
+	}
+
+	public function simpleNumberProvider() {
+		$argLists = array();
+
+		$argLists[] = array( 'I', 1 );
+		$argLists[] = array( 'V', 5 );
+		$argLists[] = array( 'X', 10 );
+		$argLists[] = array( 'L', 50 );
+		$argLists[] = array( 'C', 100 );
+		$argLists[] = array( 'D', 500 );
+		$argLists[] = array( 'M', 1000 );
+
+		return $argLists;
+	}
+
+	/**
+	 * @dataProvider numberWithRepeatedSymbolsProvider
+	 */
+	public function testFormatNumberTwo( $expected, $input ) {
+		$this->assertInputResultsInExpected( $expected, $input );
+	}
+
+	public function numberWithRepeatedSymbolsProvider() {
+		$argLists = array();
+
+		$argLists[] = array( 'II', 2 );
+		$argLists[] = array( 'III', 3 );
+		$argLists[] = array( 'XX', 20 );
+		$argLists[] = array( 'XXX', 30 );
+		$argLists[] = array( 'MM', 2000 );
+		$argLists[] = array( 'MMM', 3000 );
+
+		return $argLists;
+	}
+
+	/**
+	 * @dataProvider numbersWithFourProvider
+	 */
+	public function testFormatNumbersWithFour( $expected, $input ) {
+		$this->assertInputResultsInExpected( $expected, $input );
+	}
+
+	public function numbersWithFourProvider() {
+		$argLists = array();
+
+		$argLists[] = array( 'IV', 4 );
+		$argLists[] = array( 'XL', 40 );
+		$argLists[] = array( 'CD', 400 );
+
+		return $argLists;
+	}
+
+	/**
+	 * @dataProvider numbersWithNineProvider
+	 */
+	public function testFormatNumbersWithNine( $expected, $input ) {
+		$this->assertInputResultsInExpected( $expected, $input );
+	}
+
+	public function numbersWithNineProvider() {
+		$argLists = array();
+
+		$argLists[] = array( 'IX', 9 );
+		$argLists[] = array( 'XC', 90 );
+		$argLists[] = array( 'CM', 900 );
+
+		return $argLists;
+	}
+
+	/**
+	 * @dataProvider numbersWithMultipleDigitsProvider
+	 */
+	public function testFormatNumbersMultipleDigits( $expected, $input ) {
+		$this->assertInputResultsInExpected( $expected, $input );
+	}
+
+	public function numbersWithMultipleDigitsProvider() {
+		$argLists = array();
+
+		$argLists[] = array( 'XI', 11 );
+		$argLists[] = array( 'XIV', 14 );
+		$argLists[] = array( 'XIX', 19 );
+		$argLists[] = array( 'XIII', 13 );
+		$argLists[] = array( 'XXXIII', 33 );
+		$argLists[] = array( 'CXCI', 191 );
+		$argLists[] = array( 'MMMCMXCIX', 3999 );
+		$argLists[] = array( 'DCCCXCIX', 899 );
+
+		return $argLists;
+	}
+
+	/**
+	 * @dataProvider toBigNumberProvider
+	 */
+	public function testFormatToBigNumber( $toBigNumber ) {
+		$formatter = new Formatter();
+
+		$this->setExpectedException( 'OutOfBoundsException' );
+		$formatter->formatNumber( $toBigNumber );
+	}
+
+	public function toBigNumberProvider() {
+		$argLists = array();
+
+		$argLists[] = array( 4000 );
+		$argLists[] = array( 10000 );
+
+		return $argLists;
+	}
+
+	/**
+	 * @dataProvider toSmallNumberProvider
+	 */
+	public function testFormatToSmallNumber( $toSmallNumber ) {
+		$formatter = new Formatter();
+
+		$this->setExpectedException( 'OutOfRangeException' );
+		$formatter->formatNumber( $toSmallNumber );
+	}
+
+	public function toSmallNumberProvider() {
+		$argLists = array();
+
+		$argLists[] = array( 0 );
+		$argLists[] = array( -1 );
+		$argLists[] = array( -42 );
+		$argLists[] = array( -9001 );
+
+		return $argLists;
+	}
+
+	/**
+	 * @dataProvider invalidNumberProvider
+	 */
+	public function testFormatInvalidNumber( $invalidNumber ) {
+		$formatter = new Formatter();
+
+		$this->setExpectedException( 'InvalidArgumentException' );
+		$formatter->formatNumber( $invalidNumber );
+	}
+
+	public function invalidNumberProvider() {
+		$argLists = array();
+
+		$argLists[] = array( '1' );
+		$argLists[] = array( '0' );
+		$argLists[] = array( '42' );
+		$argLists[] = array( '4.2' );
+		$argLists[] = array( 4.2 );
+		$argLists[] = array( 4.0 );
+		$argLists[] = array( 9000.1 );
+		$argLists[] = array( 0.0001 );
+		$argLists[] = array( false );
+		$argLists[] = array( true );
+		$argLists[] = array( null );
+
+		return $argLists;
+	}
+
+	/**
+	 * @dataProvider alternateSymbolMapProvider
+	 */
+	public function testAlternateSymbolMap( $expected, $input ) {
+		$symbolMap = array(
+			array( 'A', 'B' ),
+			array( 'C', 'D' ),
+		);
+
+		$formatter = new Formatter( $symbolMap );
+
+		$result = $formatter->formatNumber( $input );
+		$this->assertEquals( $expected, $result );
+	}
+
+	public function alternateSymbolMapProvider() {
+		$argLists = array();
+
+		$argLists[] = array( 'A', 1 );
+		$argLists[] = array( 'AAA', 3 );
+		$argLists[] = array( 'AB', 4 );
+		$argLists[] = array( 'AC', 9 );
+		$argLists[] = array( 'DCCAC', 79 );
+
+		return $argLists;
+	}
+
+	/**
+	 * @dataProvider upperBoundProvider
+	 */
+	public function testGetUpperBound( array $symbolMap, $expected ) {
+		$formatter = new Formatter( $symbolMap );
+		$this->assertEquals( $expected, $formatter->getUpperBound() );
+	}
+
+	public function upperBoundProvider() {
+		$argLists = array();
+
+		$argLists[] = array(
+			array(),
+			3999
+		);
+
+		$argLists[] = array(
+			array(
+				array( 'I' ),
+			),
+			3
+		);
+
+		$argLists[] = array(
+			array(
+				array( 'I', 'V' ),
+			),
+			8
+		);
+
+		$argLists[] = array(
+			array(
+				array( 'I', 'V' ),
+				array( 'X', ),
+			),
+			39
+		);
+
+		$argLists[] = array(
+			array(
+				array( 'I', 'V' ),
+				array( 'X', 'L' ),
+			),
+			89
+		);
+
+		$argLists[] = array(
+			array(
+				array( 'A', 'B' ),
+				array( 'C', 'D' ),
+				array( 'E', 'F' ),
+			),
+			899
+		);
+
+		return $argLists;
+	}
+
+}
